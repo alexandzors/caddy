01386825b88cf5b8959757e6d56e34014d212e52
Alexander Henderson <alexandzors@gmail.com>
2024-05-31 03:14:39 -0400

    fix incorrect goarch value

--- build.sh
+++ build.sh
@@ -12,5 +12,5 @@ echo #################
 echo BUILD ARM64
 echo #################
 export GOOS=linux
-export GOARCH=arm
+export GOARCH=arm64
 xcaddy build --output ./caddy_arm64 --with github.com/WeidiDeng/caddy-cloudflare-ip --with github.com/caddy-dns/cloudflare --with github.com/caddyserver/ntlm-transport --with github.com/caddyserver/replace-response --with github.com/greenpau/caddy-security --with github.com/caddyserver/transform-encoder --with github.com/caddyserver/nginx-adapter --with github.com/mholt/caddy-ratelimit
